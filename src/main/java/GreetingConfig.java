4af62306bdf7a5778740d3dcb18e2bd3ea8bf654
Tanveer <tanvoid0@gmail.com>
2023-11-04 21:53:44 +0000

    Initial Files

---
+++ src/main/java/GreetingConfig.java
@@ -0,0 +1,11 @@
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+
+}
